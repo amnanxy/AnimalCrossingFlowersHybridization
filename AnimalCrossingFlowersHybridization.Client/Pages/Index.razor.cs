54ba0bd5489d9abf7a723b1ad1cc26d985f2ba4b
amnanxy <amnanxy@yahoo.com.tw>
2021-09-23 22:23:53 +0800

    add loading effect

--- AnimalCrossingFlowersHybridization.Client/Pages/Index.razor.cs
+++ AnimalCrossingFlowersHybridization.Client/Pages/Index.razor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using AnimalCrossingFlowersHybridization.Client.Components;
 using AnimalCrossingFlowersHybridization.Client.ViewModels.Flowers;
 using AnimalCrossingFlowersHybridization.Client.ViewModels.Indexes;
 using Microsoft.AspNetCore.Components;
@@ -8,16 +10,23 @@ namespace AnimalCrossingFlowersHybridization.Client.Pages
 {
     public class IndexBase : ComponentBase
     {
-        protected IndexViewModel IndexViewModel { get; set; } = new();
+        protected IndexViewModel IndexViewModel { get; } = new();
 
         protected IEnumerable<FlowerViewModel> FlowerViewModels { get; private set; }
 
-        protected void SearchFlower()
+        protected bool IsLoading { get; private set; }
+
+        protected async Task SearchFlower()
         {
-            var dataBank = DataBank.DataBanks[IndexViewModel.Assortment];
-            var searcher = new HybridizationPathSearcher(dataBank);
-            var flowers = searcher.SearchColor(dataBank.RareColor, IndexViewModel.Probability / 100m);
-            FlowerViewModels = new FlowerViewModelConverter().Convert(flowers);
+            IsLoading = true;
+            FlowerViewModels = await Task.Run(() =>
+            {
+                var dataBank = DataBank.DataBanks[IndexViewModel.Assortment];
+                var searcher = new HybridizationPathSearcher(dataBank);
+                var flowers = searcher.SearchColor(dataBank.RareColor, IndexViewModel.Probability / 100m);
+                return new FlowerViewModelConverter().Convert(flowers);
+            });
+            IsLoading = false;
         }
     }
 }
